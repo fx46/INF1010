d4f0bc2881f5538c7fc0e3c1bfcce63a611880f6
François-Xavier Legault <francois-xavier.legault@polymtl.ca>
2017-09-13 09:03:32 -0400

    Depot de Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,53 @@
+/**************************************************
+ * Titre: Travail pratique #1 - Main.cpp
+ * Date: Septembre 2017
+ * Auteur: 
+**************************************************/
+
+#include "Pixel.h"
+#include "Image.h"
+#include "GroupImage.h"
+
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+	//1- Creez 9 pixels rouge  (255,0,0)	
+
+	//2-Creez 9 pixels verts (0,255,0)
+	
+	//3- Creez une image de taille 3*3 
+	
+	//4- Ajouter à l'image créée à l'étape 3 les pixels rouges
+	
+	//5- Creez une deuxième image de taille 3*3 
+	
+	//6- Ajouter à l'image créées à l'étape 3 les pixels verts
+	
+	//7- Creez un groupe d'image avec une capacite de 3
+	
+	//8- Ajoutez les deux images crées precédament au groupe
+	
+	//9- Augmentez la teinte bleu du Pixel (1,1) de l'image 0 de 50 unités
+	 
+	//10- Diminuer la teinte rouge du Pixel (1, 1) de l'image 0 de 255 unités
+	
+	//11- Augmentez la teinte bleu du Pixel (2, 1) de l'image 1 de 100 unités  
+	
+	//12- Affichez les images de ce groupe
+	
+	//13- Doublez la taille de la premiere image du groupe en largeur	
+
+	//14- Doublez la taille de la deuxieme image du groupe en hauteur
+
+	//15- Afficher cette image 
+	
+	system("pause");
+	return 0;
+}
